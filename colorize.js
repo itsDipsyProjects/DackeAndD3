f983197ec22bacbddbf770136a5e7a0d18b37fa1
lodacke <lo.dacke@mail.com>
2024-03-04 14:53:58 +0100

    added excel file with names for each kommun

--- colorize.js
+++ colorize.js
@@ -2,6 +2,9 @@ import { getKommunID } from "./main.js";
 import {startViz} from "./main.js";
 
 
+d3.interpolateBlue(6)
+
+
 export async function getKommun(){
 
     let areaKommun = getKommunID();
