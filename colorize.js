761caf877c1e27d11bfa4ae827b7a04aa947dc92
itsDipsyProjects <elliot.collins.git@gmail.com>
2024-03-06 12:08:01 +0100

    updpdate

--- colorize.js
+++ colorize.js
@@ -8,84 +8,54 @@ export async function getColor(data, akutellDosEller1Dos){
 
     const colorScaleRed = d3.scaleLinear([30,100], ["yellow", "red"])
 
-
     if(akutellDosEller1Dos === "förstDos"){
-        
+
         data.forEach((item, i) => {
             const node = d3.select(`#x${item.id}`)
-                    .style("fill", colorScaleBlue(item.value))
-                    .on("mouseenter", (e) =>  {
-            
+            .on("mouseover", (e) =>  {
                         
-                         
-                        d3.select("svg").append("g").classed("hoverGroupd",true)
-                        .append("rect")
-                        .attr("width", 100)
-                        .attr("height", 50)
-                        .attr("fill", "grey")
-                        .attr("x", `${e.clientX - 170}`)
-                        .attr("y", `${e.clientY}`)
-                        d3.select("svg").append("g").classed("hoverGroupd",true)
-                        .append("text")
-                        .attr("fill", "white")
-                        .attr("x", `${e.clientX - 135}`)
-                        .attr("y", `${e.clientY + 30}`)
-                        .text(`${item.value}%`)
-                        
+                d3.select("svg").append("g").classed("hoverGroupd",true)
+                .append("rect")
+                .attr("width", 100)
+                .attr("height", 50)
+                .attr("fill", "grey")
+                .attr("x", `${e.clientX - 170}`)
+                .attr("y", `${e.clientY}`)
+                d3.select("svg").append("g").classed("hoverGroupd",true)
+                .append("text")
+                .attr("fill", "white")
+                .attr("x", `${e.clientX - 135}`)
+                .attr("y", `${e.clientY + 30}`)
+                .text(`${item.value}%`)
+            })
+            .on("mouseleave", (e) =>{
+                d3.selectAll(".hoverGroupd").remove();
+            })
+            .transition()
+            .delay(i * 4)
+            .duration(10) 
+            .style("fill", colorScaleBlue(item.value))
 
-                       
-
-                    })
-                    .on("mouseleave", (e) =>  {
-            
-                        
-                         
-                        d3.selectAll(".hoverGroupd").remove();
-                    });
-        
-        
-        })
+        });
     }
-
     if(akutellDosEller1Dos === "andraDos"){
-           
+
         data.forEach((item, i) => {
         const node = d3.select(`#x${item.id}`)
+                .on("click", (e) =>  {
+                    console.log(item.value)
+                })
                 .transition()
                 .delay(i * 4)
                 .duration(100) 
                 .style("fill", colorScaleRed(item.value))
-                .on("click", (e) =>  {
-                      
-                    d3.select("svg").append("g").classed("hoverGroupd",true)
-                    .append("rect")
-                    .attr("width", 100)
-                    .attr("height", 50)
-                    .attr("fill", "grey")
-                    .attr("x", `${e.clientX - 170}`)
-                    .attr("y", `${e.clientY}`)
-                    d3.select("svg").append("g").classed("hoverGroupd",true)
-                    .append("text")
-                    .attr("fill", "white")
-                    .attr("x", `${e.clientX - 135}`)
-                    .attr("y", `${e.clientY + 30}`)
-                    .text(`${item.value}%`)
 
-                })
-                .on("mouseleave", (e) =>  {
-            
-                        
-                         
-                    d3.selectAll(".hoverGroupd").remove();
-                    
-                })
-                    console.log(item.value)
-                })
-                 
+        });
 
-     };
+    }
 
 }
+
 
 
 
