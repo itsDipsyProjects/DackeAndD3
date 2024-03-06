d50502cb101016b03cb852e6da8c8be6e8ae0839
itsDipsyProjects <elliot.collins.git@gmail.com>
2024-03-06 10:34:26 +0100

    Update colorize.js

--- colorize.js
+++ colorize.js
@@ -1,7 +1,8 @@
 //import { areaRadial } from "d3-shape";
 
-export async function getColor(data, akutellDosEller1Dos){
 
+export async function getColor(data, akutellDosEller1Dos){
+    
     const colorScaleBlue = d3.scaleSequential(d3.interpolateBlues)
                         .domain([50, 100]); 
 
@@ -13,8 +14,33 @@ export async function getColor(data, akutellDosEller1Dos){
         data.forEach((item, i) => {
             const node = d3.select(`#x${item.id}`)
                     .style("fill", colorScaleBlue(item.value))
-                    .on("click", (e) =>  {
-                        console.log(item.value)
+                    .on("mouseenter", (e) =>  {
+            
+                        
+                         
+                        d3.select("svg").append("g").classed("hoverGroupd",true)
+                        .append("rect")
+                        .attr("width", 100)
+                        .attr("height", 50)
+                        .attr("fill", "grey")
+                        .attr("x", `${e.clientX - 170}`)
+                        .attr("y", `${e.clientY}`)
+                        d3.select("svg").append("g").classed("hoverGroupd",true)
+                        .append("text")
+                        .attr("fill", "white")
+                        .attr("x", `${e.clientX - 135}`)
+                        .attr("y", `${e.clientY + 30}`)
+                        .text(`${item.value}%`)
+
+
+                       
+
+                    })
+                    .on("mouseleave", (e) =>  {
+            
+                        
+                         
+                        d3.selectAll(".hoverGroupd").remove();
                         
                     })
         });
@@ -25,8 +51,28 @@ export async function getColor(data, akutellDosEller1Dos){
         const node = d3.select(`#x${item.id}`)
                 .style("fill", colorScaleRed(item.value))
                 .on("click", (e) =>  {
-                    console.log(item.value)
+                      
+                    d3.select("svg").append("g").classed("hoverGroupd",true)
+                    .append("rect")
+                    .attr("width", 100)
+                    .attr("height", 50)
+                    .attr("fill", "grey")
+                    .attr("x", `${e.clientX - 170}`)
+                    .attr("y", `${e.clientY}`)
+                    d3.select("svg").append("g").classed("hoverGroupd",true)
+                    .append("text")
+                    .attr("fill", "white")
+                    .attr("x", `${e.clientX - 135}`)
+                    .attr("y", `${e.clientY + 30}`)
+                    .text(`${item.value}%`)
 
+                })
+                .on("mouseleave", (e) =>  {
+            
+                        
+                         
+                    d3.selectAll(".hoverGroupd").remove();
+                    
                 })
         });
 
@@ -34,13 +80,7 @@ export async function getColor(data, akutellDosEller1Dos){
 
 }
 
-function XYcor(event){
-    const svgDom = document.querySelector("svg");
-    let point = svgDom.createSVGPoint();
-    point.x = event.clientX;
-    point.y = event.clientY;
-    return point.matrixTransform(svgDom.getScreenCTM()).inverse();
-}
+
 
 
   
